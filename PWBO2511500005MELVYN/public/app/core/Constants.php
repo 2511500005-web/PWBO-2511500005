e13b0cc1d6fd4b9359de137d401acb65f9eb3d37
2511500005-web <2511500005@mahasiswa.atmaluhur.ac.id>
2026-09-22 18:58:16 +0700

    Add files via upload

---
+++ PWBO2511500005MELVYN/public/app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php
+
+define('BASEURL', 'http://localhost/PWBO2511500005MELVYN/public');
---
+++ PWBO2511500005MELVYN/public/app/views/home/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="jumbotron">
+        <h1>Selamat Datang di Website Saya</h1>
+        <p>MELVYN 2511500005 PWBO.</p>
+        <a class="btn" href="<?php echo BASEURL; ?>/about">Learn more</a>
+    </div>
+</div>
